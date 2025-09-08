95a99eabe9f69e748fcfe168aa8b6c883d503d74
baruch9353 <baruchminzberg@gmail.com>
2025-09-08 17:54:05 +0300

    Monday end commit

--- client/src/components/application-layout/Header.tsx
+++ client/src/components/application-layout/Header.tsx
@@ -1,10 +1,18 @@
 import logo from '../../assets/IMG-20250907-WA0006.jpg'
 import '../style/Header.css'
+
+// Header component 
 export default function Header() {
     return (
         <nav>
-            <h1>Linkodkod</h1>
-            <img src={logo} className="logo" alt="logo" />
+            <h1 onClick={() => window.location.href = '/'}>Linkodkod</h1>
+            <button className='home-button' onClick={() => window.location.href = '/'}>Home</button>
+            <div className='button-group'>
+                <button>✏️</button>
+                <button>🗑️</button>
+                <button>➕</button>
+            </div>
+            <img src={logo} className="logo" alt="logo" onClick={() => window.location.href = '/'}/>
         </nav>
     );
 }
